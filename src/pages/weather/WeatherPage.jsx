c914aa338e0d1cb0c61266f01b7661d7437e5956
Savina <57533882+mashkowl@users.noreply.github.com>
2024-01-13 21:01:27 +0300

    feat(Weather): change global style and add select city logic

--- src/pages/weather/WeatherPage.jsx
+++ src/pages/weather/WeatherPage.jsx
@@ -1,45 +1,98 @@
 import '../../pages/weather/WeatherPage.css'
-// import { LoadCitiesList } from "../../store/weather.js";
-import { Button, ButtonGroup, Paper } from "@mui/material";
-import { loadCurrentWeather } from "../../store/weather.js";
+import { Autocomplete, CssVarsProvider } from "@mui/joy";
+import { loadFutureWeather, loadPresentWeather } from "../../store/weather.js";
 import { useState } from "react";
-import { format } from "date-fns";
+import { format, getISODay } from "date-fns";
+import { weekdayFormatted } from "../../helpers/date.helper.js";
+import { CITIES_LIST } from "../../consts/weatherValues.js"
 
 export default function WeatherPage() {
   const [weatherData, setWeatherData] = useState(null)
+  const [futureWeather, setFutureWeather] = useState(null)
+  const [isLoading, setLoading] = useState(false)
   function loadWeather(city) {
-    loadCurrentWeather(city).then((response) => {
-      setWeatherData(weatherData => ({...response.data}))
-      }
-    )
+    setLoading(true)
+    Promise.all([
+      loadPresentWeather(city).then((response) => {
+        setWeatherData(weatherData => ({...response.data}))
+      }),
+      loadFutureWeather(city).then((response) => {
+        setFutureWeather(futureWeather => ({...response.data}))
+      })
+    ])
+      .finally(() => {
+        setTimeout(() => {
+          setLoading(false)
+        }, 3000)
+      })
   }
 
   return (
     <div className='weather-page'>
       <h2>Simple weather widget</h2>
 
-      <ButtonGroup className="buttons" size="large" color="secondary" variant="text" aria-label="large secondary button group">
-        <Button onClick={() => loadWeather('London')}>London</Button>
-        <Button onClick={() => loadWeather('Shanghai')}>Shanghai</Button>
-        <Button onClick={() => loadWeather('Moscow')}>Moscow</Button>
-      </ButtonGroup>
+      <section className="cities-list-content">
+        <CssVarsProvider defaultMode="dark">
+          <Autocomplete
+            variant="plain"
+            placeholder="Select city"
+            id="cities-list"
+            options={CITIES_LIST}
+            onSelect={(event) => {
+              if (!!event.target.value) {
+                loadWeather(`${event.target.value}`)
+              } else {
+                setWeatherData(null)
+                setFutureWeather(null)
+              }
+            }}
+          />
+        </CssVarsProvider>
+      </section>
 
-      {weatherData && (
-        <section  className="weather-card">
-          <h3>{weatherData.location.name}, {weatherData.location.country}</h3>
-
-          <time dateTime={weatherData.location.localtime}>
-            {format(weatherData.location.localtime, 'dd.MM.yyyy H:mm')}
-          </time>
-
-          <div className='weather-card--info'>
+      {weatherData && !isLoading ? (
+        <section className="weather-content">
             <span
-              className={`${weatherData.current.is_day === 0 ? 'night-' : 'day-'}${weatherData.current.condition.icon.split('/')[6].split('.')[0]} weather-icon`}></span>
-            <p>{weatherData.current.temp_c}°C</p>
-            <p>{weatherData.current.condition.text}</p>
-          </div>
+              className={
+                `${weatherData.current.is_day === 0 ? 'night-' : 'day-'}` +
+                `${weatherData.current.condition.icon.split('/')[6].split('.')[0]} ` +
+                'weather-icon ' +
+                `weather-icon_${weatherData.current.is_day === 0 ? 'night' : 'day'}`
+              }
+            />
+          <section className={`weather-card weather-card_${weatherData.current.is_day === 0 ? 'night' : 'day'}`}>
+            <h3>{weatherData.location.name}, {weatherData.location.country}</h3>
 
+            <time dateTime={weatherData.location.localtime}>
+              {format(weatherData.location.localtime, 'dd.MM.yyyy HH:mm')}
+            </time>
+
+            <div className='weather-card--info'>
+              <div>
+                <p>{weatherData.current.temp_c}°C</p>
+                <p>feels like: {weatherData.current.feelslike_c}°C</p>
+                <p>{weatherData.current.condition.text}</p>
+              </div>
+            </div>
+
+            <div className="future-weather">
+              {futureWeather.forecast.forecastday.map((item, index) => (
+                <div key={index} className="future-weather__card">
+                  <h4>{weekdayFormatted(getISODay(item.date))}</h4>
+                  <span
+                    className={
+                      `day-${item.day.condition.icon.split('/')[6].split('.')[0]} ` +
+                      'future-weather-icon'
+                    }
+                  />
+                  <div>{item.day.mintemp_c} / {item.day.maxtemp_c}</div>
+                </div>
+              ))}
+            </div>
+          </section>
         </section>
+      ) : (
+        <section className=" weather-card weather-card_loader"/>
       )}
     </div>
   )
--- src/store/weather.js
+++ src/store/weather.js
@@ -1,6 +1,11 @@
 import axios from 'axios'
+import { format } from "date-fns";
 
 const API_KEY = 'a43825736d8445c19ed163411240301';
-export function loadCurrentWeather(city) {
+export function loadPresentWeather(city) {
   return axios.get(`http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${city}&aqi=yes`)
+}
+
+export function loadFutureWeather(city) {
+  return axios.get(`http://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${city}&days=3&aqi=no&alerts=no`)
 }
